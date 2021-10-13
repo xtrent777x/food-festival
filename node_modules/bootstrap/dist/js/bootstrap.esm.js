8a4b6bb026daaa6bbeb75bf04cff2ee5cf0b5497
Trent Roesbery <xtrent777x@gmail.com>
2021-10-12 21:50:14 -0600

    image and loader packages

--- node_modules/bootstrap/dist/js/bootstrap.esm.js
+++ node_modules/bootstrap/dist/js/bootstrap.esm.js
@@ -3,7 +3,7 @@
   * Copyright 2011-2021 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
-import * as Popper from '@popperjs/core';
+// import * as Popper from '@popperjs/core';
 
 /**
  * --------------------------------------------------------------------------
